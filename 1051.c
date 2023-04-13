de319c786abeadc7e20016e447b40cbf6ad38a24
Gil-CP <101299282+Gil-CP@users.noreply.github.com>
2023-04-13 17:13:41 -0300

    Add files via upload

---
+++ 1051.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define oito 2
+#define dezoito 18
+#define vinte_oi 28
+
+int main()
+{
+    double salario, imposto, dif;
+    scanf("%lf", &salario);
+
+    if(salario >= 0 && salario <= 2000)
+    {
+        printf("Isento\n");
+    }
+    else if(salario > 2000 && salario <= 3000)
+    {
+        dif = salario - 2000;
+
+        imposto = (dif * 0.08);
+        printf("R$ %.2lf\n", imposto);
+    }
+    else if(salario > 3000 && salario <= 4500)
+    {
+        dif = salario - 2000;
+        
+        if(dif > 1000)
+        {
+            imposto = dif * 0.;
+            dif -= 1000;
+            printf("R$ %.2lf\n", imposto);
+        }
+        else
+        {
+            imposto = dif * 0.18;
+            printf("R$ %.2lf", imposto);
+        }
+    }
+    
+}
